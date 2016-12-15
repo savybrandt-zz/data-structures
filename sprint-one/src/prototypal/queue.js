3f683313ab3f0d121d20f68087e7a8d43a95c5b3
savybrandt <savybrandt@gmail.com>
2016-12-14 16:42:27 -0800

    Finished prototypal stack, still working on queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.count = 0;
+  queue.count2 = 0;
+  queue.count3 = 0;
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.count++;
+};
 
+queueMethods.dequeue = function() {
+  this.count--;
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create( stackMethods );
+  stack.count = 0;
+  stack.storage = {};
+
+  return stack;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function() {
+  if ( this.count > 0 ) {
+    this.count--;
+  }
+  return this.storage[this.count]
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
+
+
 
 
