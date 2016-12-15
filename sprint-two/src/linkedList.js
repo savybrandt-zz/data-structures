0c2978bfef2be0fadff1d39d1a432005d47bdae9
savybrandt <savybrandt@gmail.com>
2016-12-15 14:28:27 -0800

    Still working on the same problem

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,25 +2,28 @@ var LinkedList = function() {
   var list = {};
   list.head = {};
   list.tail = {};
+  list.next = {};
 
   list.addToTail = function(value) {
     var findTail = function(node) {
-      if ( node.next ) {
-        findTail(node.next);
-      } else {
-        node.next = Node(value);        
+      if ( node.next ) { // Does this node have something to point to / is it last node?
+        findTail(node.next); // If it's not last node, then perform recursion on the node that current node is pointing to.
+      } else { // If this node is the last node,
+        node = Node(value); // add to tail by using Node function
+        if ( !list.head ) { //  If first time through, then 
+          list.head = node; // Assign the first node as the head node, too
+        }
+        list.tail = node; // Reassign new tail to list.tail
       }
-      return node;
     };
-    list = findTail(list);
-    list.tail.value = value;
+    findTail(list.next); // Initiating recursion with current head
   };
 
-  list.removeHead = function() {
-    var results = list.next.value;
+  list.removeHead = function() {  
+    var removedHead = list.head.value; // Declare previous head value
+    list.head = list.next; // Declare new head value
     list.next = list.next.next;
-    list.head.value = list.next.value;
-    return results;
+    return removedHead;
   };
 
   list.contains = function(target) {
@@ -47,3 +50,9 @@ var Node = function(value) {
 // .removeHead() method, removes the first node from the list and returns its value
 // .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
 // What is the time complexity of the above functions?
+
+// linkedList.addToTail(4);
+// linkedList.addToTail(5);
+// expect(linkedList.head.value).to.equal(4);
+// linkedList.removeHead();
+// expect(linkedList.head.value).to.equal(5);
