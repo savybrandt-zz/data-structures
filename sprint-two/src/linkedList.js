4c85e9a04171fa9296ce8eb3dd5b3dbf082cd5c7
savybrandt <savybrandt@gmail.com>
2016-12-15 11:43:21 -0800

    Working on linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,26 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = {};
+  list.tail = {};
 
   list.addToTail = function(value) {
+    var findTail = function(node) {
+      if ( node.next ) {
+        findTail(node.next);
+      } else {
+        node.next = Node(value);        
+      }
+      return node;
+    };
+    list = findTail(list);
+    list.tail.value = value;
   };
 
   list.removeHead = function() {
+    var results = list.next.value;
+    list.next = list.next.next;
+    list.head.value = list.next.value;
+    return results;
   };
 
   list.contains = function(target) {
@@ -19,11 +33,17 @@ var Node = function(value) {
   var node = {};
 
   node.value = value;
-  node.next = null;
-
+  node.next = {};
   return node;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+// An itinerary you expect to add and remove destinations to and from.
+
+// A linkedList class, in functional style, with the following properties:
+// .head property, a linkedListNode instance
+// .tail property, a linkedListNode instance
+// .addToTail() method, takes a value and adds it to the end of the list
+// .removeHead() method, removes the first node from the list and returns its value
+// .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
+// What is the time complexity of the above functions?
