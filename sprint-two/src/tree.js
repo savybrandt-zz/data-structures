e429cc41ab859c77d39e4bcf74845063c4bfafdc
savybrandt <savybrandt@gmail.com>
2016-12-16 10:31:33 -0800

    Finished Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -12,19 +12,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  var child = {value: value};
+  var child = {value: value, children: []};
   this.children.push(child);
-  console.log(this); 
-  console.log(this.children);
-  debugger;
+  child.addChild = treeMethods.addChild;
 };
 
 treeMethods.contains = function(target) {
+  var flag = false;
   var searchTarget = function(node) {
     var childrenArr = node.children;
-    for ( var i = 0; i < childrenArr.length; 
+
+    for (var i = 0; i < childrenArr.length; i++) {
+      if (target === childrenArr[i].value) {
+        flag = true;
+      }
+      if (childrenArr[i].children) {
+        searchTarget(childrenArr[i]);
+      }
+    }
   };
+
   searchTarget(this);
+  return flag;
 };
 
 // tree.addChild(5);
