3d461027fdbe839308e20c6214b08e3a9427901c
savybrandt <savybrandt@gmail.com>
2016-12-15 21:17:12 -0800

    Finish graph and set 9:17 PM THU

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,35 +2,77 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = true;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var flag = false;
+  if ( this.nodes[node] ) {
+    flag = true;
+  }
+  return flag;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.nodes[node];
+  var a = this.edges;
+  var keys = Object.keys(this.edges);
+  for ( var i = 0; i < keys.length; i++ ) {
+    if ( keys[i][0] === String(node) || keys[i][1] === String(node) ) {
+      console.log(keys[i]);
+      delete this.edges[keys[i]];
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var flag = false;
+  if (this.edges[String(fromNode) + String(toNode)] || this.edges[String(toNode) + String(fromNode)]) {
+    flag = true;
+  }
+  return flag;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[String(fromNode) + String(toNode)] = true;
+  this.edges[String(toNode) + String(fromNode)] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.edges[String(fromNode) + String(toNode)] = false;
+  this.edges[String(toNode) + String(fromNode)] = false;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for ( var key in this.nodes ) {
+    cb(key); 
+  }
 };
+
+// var connectToFive = function(item) {
+//   graph.addEdge(item, 5);
+// };
+// graph.addNode(5);
+// graph.addNode(2);
+// graph.addNode(1);
+// graph.addNode(3);
+// graph.forEachNode(connectToFive);
+// expect(graph.hasEdge(2, 5)).to.equal(true);
+// expect(graph.hasEdge(1, 5)).to.equal(true);
+// expect(graph.hasEdge(3, 5)).to.equal(true);
+// expect(graph.hasEdge(5, 5)).to.equal(true);
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if ( this._storage[item] === undefined ) {
+    this._storage[item] = true;
+  } 
 };
 
 setPrototype.contains = function(item) {
+  var flag = false;
+  if ( this._storage[item] ) {
+    flag = true; 
+  }
+  return flag;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// set.add('Susan Sarandon');
+// set.add('Danny Glover');
+// expect(set.contains('Danny Glover')).to.equal(true);
+// expect(set.contains('Susan Sarandon')).to.equal(true);
