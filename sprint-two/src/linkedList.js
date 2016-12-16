1869cec2ff8b6a753245e41153e6720f5a8dc9c9
savybrandt <savybrandt@gmail.com>
2016-12-15 16:42:09 -0800

    Reassessing our approach 4:42PM THU

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,28 +1,32 @@
 var LinkedList = function() {
   var list = {};
-  list.head = {};
-  list.tail = {};
-  list.next = {};
+  list.head = null;
+  list.tail = null;
 
   list.addToTail = function(value) {
+    
     var findTail = function(node) {
-      if ( node.next ) { // Does this node have something to point to / is it last node?
+      if ( node ) { // Does this node have something to point to / is it last node?
         findTail(node.next); // If it's not last node, then perform recursion on the node that current node is pointing to.
       } else { // If this node is the last node,
         node = Node(value); // add to tail by using Node function
         if ( !list.head ) { //  If first time through, then 
           list.head = node; // Assign the first node as the head node, too
         }
-        list.tail = node; // Reassign new tail to list.tail
+        list.tail.value = node.value; // Reassign new tail to list.tail
       }
     };
-    findTail(list.next); // Initiating recursion with current head
+    
+    
+
+    findTail(list.head); // Initiating recursion with current head
   };
 
   list.removeHead = function() {  
+    console.log(list);
     var removedHead = list.head.value; // Declare previous head value
-    list.head = list.next; // Declare new head value
-    list.next = list.next.next;
+    list.head.value = list.head.next;
+    // list.next = list.next.next;
     return removedHead;
   };
 
@@ -36,7 +40,7 @@ var Node = function(value) {
   var node = {};
 
   node.value = value;
-  node.next = {};
+  node.next = null;
   return node;
 };
 
