551c1464a1b4d8bd973306c4a4dfc4b4633d8d35
savybrandt <savybrandt@gmail.com>
2016-12-17 16:47:05 -0800

    refactored graph and binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -50,4 +50,7 @@ var BinarySearchTree = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: O(n)
+ * contains: O(n)
+ * depthFirstLog: O(n)
  */
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -12,10 +12,7 @@ Graph.prototype.addNode = function(node) {
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  if ( this.storage[node].value ) {
-    return true;
-  }
-  return false;
+  return !!this.storage[node].value;
 };
 
 // Removes a node from the graph.
@@ -26,11 +23,7 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if ( _.indexOf( this.storage[fromNode].edge, toNode ) !== -1 && _.indexOf( this.storage[toNode].edge, fromNode ) !== -1 ) {
-    return true;
-  } else {
-    return false;
-  }
+  return !!( _.indexOf( this.storage[fromNode].edge, toNode ) !== -1 && _.indexOf( this.storage[toNode].edge, fromNode ) !== -1 );
 };
 
 // Connects two nodes in a graph by adding an edge between them.
@@ -51,19 +44,6 @@ Graph.prototype.forEachNode = function(cb) {
     cb(this.storage[key].value);
   }
 };
-
-// var connectToFive = function(item) {
-//   graph.addEdge(item, 5);
-// };
-// graph.addNode(5);
-// graph.addNode(2);
-// graph.addNode(1);
-// graph.addNode(3);
-// graph.forEachNode(connectToFive);
-// expect(graph.hasEdge(2, 5)).to.equal(true);
-// expect(graph.hasEdge(1, 5)).to.equal(true);
-// expect(graph.hasEdge(3, 5)).to.equal(true);
-// expect(graph.hasEdge(5, 5)).to.equal(true);
 
 /*
  * Complexity: What is the time complexity of the above functions?
